46c8310ae300fcf3a20f35e02b4eabeceb56d2c6
dotcoin-dev <dotcoin@live.com>
2013-06-27 00:22:44 +0800

    modify

--- src/base58.h
+++ src/base58.h
@@ -278,7 +278,7 @@ class CCosmoscoinAddress : public CBase58Data
 public:
     enum
     {
-        PUBKEY_ADDRESS = 35, // Floriscoin addresses start with F
+        PUBKEY_ADDRESS = 27, // Floriscoin addresses start with F
         SCRIPT_ADDRESS = 8,
         PUBKEY_ADDRESS_TEST = 115,
         SCRIPT_ADDRESS_TEST = 198,
