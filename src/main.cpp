9153f01f57e0ab257b4dc5881fcab9d1cd803d49
cosmoscoin <cosmoscoin.com@gmail.com>
2013-06-27 18:49:22 +0800

    modify

--- src/main.cpp
+++ src/main.cpp
@@ -2558,7 +2558,7 @@ bool LoadBlockIndex(bool fAllowNew)
         txNew.vout.resize(1);
         txNew.vin[0].scriptSig = CScript() << 486604799 << CBigNum(9999) << vector<unsigned char>((const unsigned char*)pszTimestamp, (const unsigned char*)pszTimestamp + strlen(pszTimestamp));
         txNew.vout[0].SetEmpty();
-        txNew.vout[0].scriptPubKey = CScript() << ParseHex(pszMainKey) << OP_CHECKSIG;
+        //txNew.vout[0].scriptPubKey = CScript() << ParseHex(pszMainKey) << OP_CHECKSIG;
         txNew.strTxComment = "text:Cosmoscoin genesis block";
 
 
